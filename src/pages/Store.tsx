0c32595763b52b55f57a2f56c687dff242a6f88d
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2025-11-10 17:24:05 -0500

    Updated Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,15 +1,13 @@
 function Store() {
   return (
-    <div className="min-h-screen bg-white">
-      <div className="w-full" style={{ height: 'calc(100vh - 64px)', marginTop: '64px' }}>
-        <iframe
-          src="https://daccurso-career-studio-shop.fourthwall.com"
-          className="w-full h-full border-0"
-          title="Daccurso Career Studio Store"
-          allow="payment; fullscreen"
-          sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox"
-        />
-      </div>
+    <div className="w-full" style={{ height: 'calc(100vh - 64px)' }}>
+      <iframe
+        src="https://daccurso-career-studio-shop.fourthwall.com"
+        className="w-full h-full border-0"
+        title="Daccurso Career Studio Store"
+        allow="payment; fullscreen"
+        sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox"
+      />
     </div>
   );
 }
