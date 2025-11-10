680cc8ae08d743242225514e77de8065eda76c0a
Anthony Daccurso <153415273+anthonydaccurso@users.noreply.github.com>
2025-11-10 17:20:16 -0500

    Updated Store.tsx

---
+++ src/pages/Store.tsx
@@ -0,0 +1,20 @@
+import Navigation from '../components/Navigation';
+
+function Store() {
+  return (
+    <>
+      <Navigation />
+      <div className="w-full" style={{ height: 'calc(100vh - 64px)' }}>
+        <iframe
+          src="https://daccurso-career-studio-shop.fourthwall.com"
+          className="w-full h-full border-0"
+          title="Daccurso Career Studio Store"
+          allow="payment; fullscreen"
+          sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-popups-to-escape-sandbox"
+        />
+      </div>
+    </>
+  );
+}
+
+export default Store;
